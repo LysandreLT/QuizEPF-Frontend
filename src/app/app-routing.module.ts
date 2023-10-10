f9fdd63df545d61b2dfaa2ae2b3af00415ba2526
Chambellan <lysandre.letohic@epfedu.fr>
2023-10-10 15:07:48 +0200

    fix Results

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import {RouterModule, Routes} from "@angular/router";
 import {QuizQuestionComponent} from "./quiz-question/quiz-question.component";
 import {LogInComponent} from "./log-in/log-in.component";
 import {RegisterComponent} from "./register/register.component";
+import {ResultsComponent} from "./results/results.component";
 
 const routes: Routes = [
   { path: "question", component: QuizQuestionComponent },
+  { path: "results", component: ResultsComponent },
   { path: "login", component: LogInComponent },
   { path: "register", component: RegisterComponent },
 ]
